61835b6ec618fae8f7ad34a6c1ebc1cae50772bb
lucasemanuel <gaitonlucas7@gmail.com>
2024-07-23 23:03:18 -0300

    Back finalizado

--- Api/src/Controllers/taskController.js
+++ Api/src/Controllers/taskController.js
@@ -1,6 +1,6 @@
 import { Task } from "../Models/Task.js";
 
-export const getTasks= async (req, res) => {
+export const getTasks = async (req, res) => {
     try {
         const tasks = await Task.findAll();
         res.json(tasks)
@@ -9,26 +9,58 @@ export const getTasks= async (req, res) => {
 
     }
 }
-export const addTask = async(req, res) =>{
+export const addTask = async (req, res) => {
     try {
         const newTask = await Task.create(req.body)
         res.json(newTask);
     } catch (error) {
         console.log(error.message);
-        
+
     }
 }
-export const getTasKById = async (req, res) =>{
+export const getTasKById = async (req, res) => {
     try {
         const taskId = req.params.id;
         const task = await Task.findByPk(taskId)
-        if(!task){
+        if (!task) {
             return res.status(404).json({ error: 'Task not found' });
         }
         res.json(task);
     } catch (error) {
-        res.status(500).json({error:error.message});
+        res.status(500).json({ error: error.message });
+
+    }
+}
+export const putTaskById = async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        console.log(taskId);
+        console.log("aca esta la wea",req.body);
+        await Task.update(req.body, {
+            where: {
+                id: taskId
+            }
+        })
+        res.send("Se actualizo la tarea")
+    } catch (error) {
+        res.status(500).send("Error")
+
+    }
+}
+export const deleteTaskById = async (req,res) =>{
+    try {
+        const taskId = req.params.id;
+        console.log(taskId);
+        await Task.destroy({
+            where: {
+                id: taskId
+            }
+        })
+        res.send("Se actualizo la tarea")
+    } catch (error) {
+        res.status(500).send(error.message);
         
     }
+
 }
 
--- Api/src/Routes/tasksRoutes.js
+++ Api/src/Routes/tasksRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 //Controllers
-import { addTask, getTasKById, getTasks } from "../Controllers/taskController.js";
+import { addTask, deleteTaskById, getTasKById, getTasks, putTaskById } from "../Controllers/taskController.js";
 const router = Router();
 router.get("/tasks", getTasks)
 router.get("/tasks/:id", getTasKById);
+router.put("/tasks/:id", putTaskById);
+router.delete("/tasks/:id", deleteTaskById);
+
 router.post("/addTask", addTask)
 
 export default router;
